3df08103ee21dfeead90a7f23c315485017b54fd
Kevin Montalva <kevmon@wikimedia.org>
2025-07-17 16:33:54 +0200

    Update code and documentation for latest batches API changes

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -19,7 +19,7 @@ import (
 )
 
 // DateFormat is the date format used for the API.
-const DateFormat = "2006-01-02"
+const DateFormat = "2006-01-02/15"
 
 // Filter represents a filter to be applied to a dataset.
 type Filter struct {
@@ -679,29 +679,29 @@ func (c *Client) GetNamespace(ctx context.Context, idr int, req *Request) (*Name
 	return nsp, c.getEntity(ctx, req, fmt.Sprintf("namespaces/%d", idr), nsp)
 }
 
-// GetBatches retrieves a list of batches for a specific date and request, and returns an error if any.
+// GetBatches retrieves a list of batches for a specific date, hour and request, and returns an error if any.
 func (c *Client) GetBatches(ctx context.Context, dte *time.Time, req *Request) ([]*Batch, error) {
 	bts := []*Batch{}
 	return bts, c.getEntity(ctx, req, fmt.Sprintf("batches/%s", dte.Format(DateFormat)), &bts)
 }
 
-// GetBatch retrieves a single batch for a specific date and ID, and returns an error if any.
+// GetBatch retrieves a single batch for a specific date, hour and ID, and returns an error if any.
 func (c *Client) GetBatch(ctx context.Context, dte *time.Time, idr string, req *Request) (*Batch, error) {
 	bth := new(Batch)
 	return bth, c.getEntity(ctx, req, fmt.Sprintf("batches/%s/%s", dte.Format(DateFormat), idr), bth)
 }
 
-// HeadBatch retrieves only the headers of a single batch for a specific date and ID, and returns an error if any.
+// HeadBatch retrieves only the headers of a single batch for a specific date, hour and ID, and returns an error if any.
 func (c *Client) HeadBatch(ctx context.Context, dte *time.Time, idr string) (*Headers, error) {
 	return c.headEntity(ctx, fmt.Sprintf("batches/%s/%s/download", dte.Format(DateFormat), idr))
 }
 
-// ReadBatch reads the contents of a single batch for a specific date and ID, and invokes the specified callback function for each chunk read.
+// ReadBatch reads the contents of a single batch for a specific date, hour and ID, and invokes the specified callback function for each chunk read.
 func (c *Client) ReadBatch(ctx context.Context, dte *time.Time, idr string, cbk ReadCallback) error {
 	return c.readEntity(ctx, fmt.Sprintf("batches/%s/%s/download", dte.Format(DateFormat), idr), cbk)
 }
 
-// DownloadBatch downloads the contents of a single batch for a specific date and ID, and writes the data to the specified WriteSeeker.
+// DownloadBatch downloads the contents of a single batch for a specific date, hour and ID, and writes the data to the specified WriteSeeker.
 func (c *Client) DownloadBatch(ctx context.Context, dte *time.Time, idr string, wsk io.WriteSeeker) error {
 	return c.downloadEntity(ctx, fmt.Sprintf("batches/%s/%s/download", dte.Format(DateFormat), idr), wsk)
 }
